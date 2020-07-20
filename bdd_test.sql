08c24e9adf5b6b9cd5ee39c29c460513c74bbb6f
BOURAHIM <66995559+BOURAHIM@users.noreply.github.com>
2020-07-20 16:47:09 +0200

    Update bdd_test.sql
    MAJ

--- bdd_test.sql
+++ bdd_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 20 juil. 2020 à 16:28
+-- Généré le : lun. 20 juil. 2020 à 16:46
 -- Version du serveur :  10.4.13-MariaDB
 -- Version de PHP : 7.4.8
 
